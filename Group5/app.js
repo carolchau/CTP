9651260abd73d6bb38eb281140564b1f23c556c0
Yaoming Jiang <yaomingjiang@Yaomings-MacBook-Pro.local>
2016-12-07 18:01:32 -0500

    included location route

--- Group5/app.js
+++ Group5/app.js
@@ -19,6 +19,7 @@ var db = mongoose.connection;
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var reviews = require('./routes/reviews');
+var locations = require('./routes/locations');
 
 // Initialize App
 var app = express();
@@ -84,6 +85,7 @@ app.use(function(req, res, next){
 app.use('/', routes);
 app.use('/users', users);
 app.use('/reviews', reviews);
+app.use('/locations', locations);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
